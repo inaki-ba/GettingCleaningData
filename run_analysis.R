ac34738c07a99dd9b5d9b2a028d429e2de05793d
Inaki <inaki.blanco@gmail.com>
2018-03-17 22:23:44 +0100

    Getting and Cleaning Data assignment- Includes Data-book and README.md file

--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,28 @@
 run_analysis <- function() {
 	
-	##
-	l_activities <-read.table("wear/activity_labels.txt")
-	t_activities<-read.table("wear/features.txt")
+	## Sets up vectors, including activity lables, and subsets of variables: means and stds
+	l_activities <-read.table("activity_labels.txt")
+	t_activities<-read.table("features.txt")
 	i_activities<-grep("mean|std", t_activities$V2)
 	s_activities<-grep("mean|std", t_activities$V2, value = TRUE)
 	
-	##
-	t_train<-read.table("wear/train/X_train.txt")
-	t_train_lab<-read.table("wear/train/y_train.txt")
-	t_train_sub<-read.table("wear/train/subject_train.txt")
+	## Shapes train sets, including column nanes, activity names for the activiy variable
+	t_train<-read.table("train/X_train.txt")
+	t_train_lab<-read.table("train/y_train.txt")
+	t_train_sub<-read.table("train/subject_train.txt")
 	d_train<-data.frame(t_train_sub, t_train_lab, t_train[,i_activities])
 	colnames(d_train) <- c("Subject", "Activity", c(s_activities))
 	d_train$Activity<-l_activities$V2[d_train$Activity]
 	
-	##
-	t_test<-read.table("wear/test/X_test.txt")
-	t_test_lab<-read.table("wear/test/y_test.txt")
-	t_test_sub<-read.table("wear/test/subject_test.txt")
+	## Shapes test sets, including column nanes, activity names for the activiy variable
+	t_test<-read.table("test/X_test.txt")
+	t_test_lab<-read.table("test/y_test.txt")
+	t_test_sub<-read.table("test/subject_test.txt")
 	d_test<-data.frame(t_test_sub, t_test_lab, t_test[,i_activities])
 	colnames(d_test) <- c("Subject", "Activity", c(s_activities))
 	d_test$Activity<-l_activities$V2[d_test$Activity]
 	
-	##	
+	##	Merges the two sets, and calculates the summary set with averages per activity/subject
 	final_frame<-rbind("Train"=d_train, "Test"=d_test)
 	k<-1
 	final_frame2<-final_frame[1,]
@@ -34,5 +34,7 @@ run_analysis <- function() {
 			k<-k+1
 		}
 	}
+
+	##	Returns the summary set: 180 rows, 81 columns
 	final_frame2
 }
